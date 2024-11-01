17754e2574e3204426f9aaebbb7111fd904f8170
Christofer <chhernandezga@est.utn.ac.cr>
2024-11-01 01:10:17 -0600

    Creacion Frm y Iterface Iview

---
+++ Vista/IView.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Vista;
+
+/**
+ *
+ * @author chris
+ */
+public interface IView {
+    public void showMessage(String message);
+    public void show(Object object);
+}
